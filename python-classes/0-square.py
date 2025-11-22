d61adca289638fe2c10ee8a22f78c7e6409951ad
aliqulamaliyev <aliqulamaliyev@gmail.com>
2025-11-22 07:15:12 +0000

    copletes all python tsaks

---
+++ python-classes/0-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+class Square:
+    """Defines a square"""
+
+    def __init__(self, size):
+        """Initialization of the square with a private size attribute"""
+        self.__size = size
+
